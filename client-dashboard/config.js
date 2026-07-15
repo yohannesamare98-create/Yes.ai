84a27746ab790f5c73c0f76ee50865e75cd0ca13
yohannesamare98-create <yohannesamare98@gmail.com>
2026-07-15 16:52:54 +0400

    Update Supabase URL in config.js

--- client-dashboard/config.js
+++ client-dashboard/config.js
@@ -3,6 +3,6 @@
 // Leave blank to keep running in DEMO MODE.
 
 window.YESAI_CONFIG = {
-  SUPABASE_URL: 'https://wrwbslqnqgdkmlrfaojw.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://wrwbslqnqgdkmlrfaojw.supabase.co',
   SUPABASE_ANON_KEY: sb_publishable_xUQ45iNy66RkVYnwV2IxbA_92WsiWHn''
 };
